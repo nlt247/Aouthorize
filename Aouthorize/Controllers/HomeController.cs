426b9f2fd9ad8b95b8480eba6f4c98b2b31c553a
dzh247 <2476254969@qq.com>
2020-12-19 11:24:22 +0800

    添加IdentityExample

--- Aouthorize/Controllers/HomeController.cs
+++ Aouthorize/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +17,30 @@ namespace Aouthorize.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [Authorize]
+        public IActionResult Secret() {
+            return View();
+        }
+
+        public IActionResult Authenticate() {
+            var grandmaclaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob"),
+                new Claim(ClaimTypes.Email,"Bob@fmail.com"),
+                new Claim("Grandma.Says","Very nice boi.")
+            };
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob K Foo"),
+                new Claim("DrivingLicense","A+")
+            };
+            var grandmaIdentity = new ClaimsIdentity(grandmaclaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity,licenseIdentity });
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Aouthorize/Startup.cs
+++ Aouthorize/Startup.cs
@@ -16,6 +16,12 @@ namespace Aouthorize
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth",config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+                });
             services.AddControllersWithViews();
         }
 
@@ -26,9 +32,10 @@ namespace Aouthorize
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
